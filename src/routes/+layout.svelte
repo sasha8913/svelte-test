a504d1dee120d3a1449f64b13d640af0a68027a8
sasha8913 <delsuka3000@gmail.com>
2024-03-20 03:13:50 +0700

    add blog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,9 @@
         <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
         <div class="nav-links">
             {#each nav as link}
-                <a href={link.href} class="link">{link.title}</a>
+                {#if link.isShow}
+                    <a href={link.href} class="link">{link.title}</a>
+                {/if}
             {/each}
         </div>
     </div>
@@ -17,22 +19,27 @@
         {
             title: "Обо мне",
             href: "/about",
+            isShow: true,
         },
         {
             title: "Блог",
             href: "/blog",
+            isShow: true,
         },
         {
             title: "Портфолио",
             href: "/project",
+            isShow: true,
         },
         {
             title: "Контакты",
             href: "/contact",
+            isShow: true,
         },
         {
             title: "temp",
             href: "/temp",
+            isShow: true
         },
     ]
 </script>
---
+++ src/routes/blog/posts/2/+page.svelte
@@ -0,0 +1,1 @@
+<h1>My Second Blog Post</h1>
