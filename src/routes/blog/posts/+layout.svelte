0d20e341522dc51f95e3d6013980a521f210b042
sasha8913 <delsuka3000@gmail.com>
2024-03-20 05:21:33 +0700

    add todo app

--- src/routes/blog/posts/+layout.svelte
+++ src/routes/blog/posts/+layout.svelte
@@ -1,12 +1,20 @@
 <div class="container">
-    <a href="/blog">Домой</a>
+    <a href="/blog" class="linkr">Домой</a>
     <slot />
     <div>
         <style>
             .container {
-                margin: 50px auto;
+                max-width: 1320px!important;
+                margin: 50px auto!important;
                 max-width: 800px;
                 padding: 20px;
+            }
+
+            .linkr {
+                color: rgb(10, 10, 139);
+                background: #ff9800;
+                padding: 10px;
+                border-radius: 10px;
             }
         </style>
     </div>
