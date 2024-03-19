9eeb91c299640072c3a680ae5d57169da46f3c88
sasha8913 <delsuka3000@gmail.com>
2024-03-20 03:01:58 +0700

    add pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,46 @@
-<nav>  
-    <div class="nav-container">  
+<nav>
+    <div class="nav-container">
         <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
-        <div class="nav-links">  
-            <a href="/about" class="link">Обо мне</a>  
-            <a href="/blog" class="link">Блог</a>  
-            <a href="/projects" class="link">Портфолио</a>  
-            <a href="/contact" class="link">Контакты</a>  
-        </div>  
-    </div>  
-</nav>  
-<div class="container">  
-    <!-- Pages will be injected below -->  
-    <slot></slot>  
-</div>  
-<div>  
-    <style>  
-        .container {  
-            max-width: 1400px;  
-            margin: 50px auto;  
-        }  
-    </style>  
+        <div class="nav-links">
+            {#each nav as link}
+                <a href={link.href} class="link">{link.title}</a>
+            {/each}
+        </div>
+    </div>
+</nav>
+<div class="container">
+    <!-- Pages will be injected below -->
+    <slot />
+</div>
+<script>
+    const nav = [
+        {
+            title: "Обо мне",
+            href: "/about",
+        },
+        {
+            title: "Блог",
+            href: "/blog",
+        },
+        {
+            title: "Портфолио",
+            href: "/project",
+        },
+        {
+            title: "Контакты",
+            href: "/contact",
+        },
+        {
+            title: "temp",
+            href: "/temp",
+        },
+    ]
+</script>
+<div>
+    <style>
+        .container {
+            max-width: 1400px;
+            margin: 50px auto;
+        }
+    </style>
 </div>
